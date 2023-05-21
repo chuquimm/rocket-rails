9404bd315e2ce92d0f718127d3539ffb353406ad
Marcos Chuquicondor <marcoschuquicondor@gmail.com>
2023-05-21 16:35:27 -0500

    feat(resource_route): add modules logic

--- lib/generators/resource_route/resource_route_generator.rb
+++ lib/generators/resource_route/resource_route_generator.rb
@@ -17,27 +17,53 @@ module Rails
       def add_resource_route
         return if options[:actions].present?
 
-        insert_basic_route
         create_route_file
+        insert_basic_route
       end
 
       private
 
       def insert_basic_route
         inject_into_file 'config/routes.rb', before: 'end' do
-          "  extend #{route_class_name}\n"
+          rebase_indentation("extend #{route_class_name}\n", 2)
         end
       end
 
       def route_class_name
-        "#{[regular_class_path, file_name.pluralize.camelcase].flatten.map(&:camelcase).join('')}Routes"
+        "#{[regular_class_path, file_name.pluralize.camelcase].flatten.map(&:camelcase).join('::')}Routes"
       end
 
       def create_route_file
+        template 'route.template', route_path
+        return if behavior == :revoke
+
+        route "resources :#{file_name.pluralize}", namespace: regular_class_path
+      end
+
+      # Original: https://github.com/rails/rails/blob/ef04fbb3b256beececfa44c47c4ec93ac6945e59/railties/lib/rails/generators/rails/resource_route/resource_route_generator.rb
+      # Modified: use a method (route_path) instead of 'config/routes.rb'
+      # Make an entry in Rails routing file <tt>config/routes.rb</tt>
+      #
+      #   route "root 'welcome#index'"
+      #   route "root 'admin#index'", namespace: :admin
+      def route(routing_code, namespace: nil)
+        namespace = Array(namespace)
+        routing_code = namespace.reverse.reduce(routing_code) do |code, name|
+          "namespace :#{name} do\n#{rebase_indentation(code, 2)}end"
+        end
+
+        log :route, routing_code
+
+        in_root do
+          routing_code = rebase_indentation(routing_code, 6)
+
+          inject_into_file route_path, routing_code, after: "router.instance_exec do\n", verbose: false, force: false
+        end
+      end
+
+      def route_path
         dir = ['config', 'routes', regular_class_path].join('/')
-        template 'route.template', "#{dir}/#{file_name.pluralize.underscore}_routes.rb"
-        # TODO: usar el metodo 'route' que se usa por defecto en el generador, pero dentro del template.
-        # route "resources :#{file_name.pluralize}", namespace: regular_class_path
+        "#{dir}/#{file_name.pluralize.underscore}_routes.rb"
       end
     end
   end
